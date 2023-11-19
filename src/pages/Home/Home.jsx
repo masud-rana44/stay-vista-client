63da33ac21011c8b191b2932ab2b63196a5038ca
masud-rana44 <mdmasudrana4488@gmail.com>
2023-11-19 23:41:30 +0600

    fetch rooms from fake data

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    const getRooms = async () => {
+      const res = await fetch("/rooms.json");
+      const data = await res.json();
+      setRooms(data);
+    };
+
+    getRooms();
+  }, []);
+
   return (
     <div>
       <h1>Welcome to StayVista</h1>
+      <div>
+        {rooms.map((room) => (
+          <div key={room._id}>{room.name}</div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
